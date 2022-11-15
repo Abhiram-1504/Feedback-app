ab21c52c388488a14beb4acfb0b7ded35da68db6
Abhiram <abhiramkotana154@gmail.com>
2022-11-15 10:30:41 +0530

    'initial'

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'loremIncididunt consectetur eiusmod sint occaecat minim elit tempor laboris ullamco ut aliqua.',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'loremFugiat sunt dolore labore enim quis veniam ut Lorem veniam est nisi elit aliqua dolore.',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'loremIpsum excepteur ut dolore duis incididunt fugiat dolore ea dolor excepteur quis ullamco.',
+    },
+]
+
+export default FeedbackData
